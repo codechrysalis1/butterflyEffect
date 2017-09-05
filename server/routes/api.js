5c3a1b4d57d8972c1b35251ed69b82042581900a
Yan Fan <theyanfan@gmail.com>
2017-09-05 20:19:07 +0900

    #5 Add scaffolding and lint and contribution info to README. Add eslint and stylelint set up files. Change and add test commands to package.json.

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json({ page: "Api" });
+router.get('/', (req, res, next) => {
+  res.json({ page: 'Api' });
 });
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -2,18 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { Route, BrowserRouter } from 'react-router-dom'
+import { Route, BrowserRouter } from 'react-router-dom';
 
 import './components/styles/index.css';
 import App from './components/App';
 
 import reducer from './reducer';
+
 const store = createStore(reducer);
 
+console.log(
+  'hello world',
+);
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <Route exact path="/" component={App} />
     </BrowserRouter>
-  </Provider>, document.getElementById('root')
+  </Provider>, document.getElementById('root'),
 );
