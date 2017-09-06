b3f06f403721f9f608209acdbda5f01c7e2f81a6
Bill Feng <mr.billfeng@gmail.com>
2017-09-07 02:00:00 +0900

    Make footer prettier

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import Divider from 'material-ui/Divider';
 
 const Footer = () => (
-  <footer style={{ height: '10%', display: 'flex', alignItems: 'center' }}>
-    Code Chrysalis © 2017
+  <footer className="restrict-width"
+    style={{
+      
+      height: '10%',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+  >
+    <Divider style={{ width: '100%', marginTop: '10px' }} />
+    <p style={{ display: 'flex', flexGrow: 1, alignItems: 'center', }}>Code Chrysalis © 2017</p>
   </footer>
 );
 
