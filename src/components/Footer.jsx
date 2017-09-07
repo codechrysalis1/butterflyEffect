27b1c75918ce6666d8341ff0d4c04a40847090c1
Bill Feng <mr.billfeng@gmail.com>
2017-09-07 15:40:55 +0900

    Add Footer css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import Divider from 'material-ui/Divider';
 
+import './styles/footer.css';
+
 const Footer = () => (
-  <footer
-    className="restrict-width"
-    style={{
-      height: '10%',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-    }}
-  >
-    <Divider style={{ width: '100%', marginTop: '10px' }} />
-    <p style={{ display: 'flex', flexGrow: 1, alignItems: 'center' }}>Code Chrysalis © 2017</p>
+  <footer className="restrict-width">
+    <Divider className="footer-divider" />
+    <p>Code Chrysalis © 2017</p>
   </footer>
 );
 
