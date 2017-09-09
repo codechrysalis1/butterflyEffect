2c7646740a97de39cddf74177bd2421e51fc81ac
Michael Sodaro <xtra.zero@gmail.com>
2017-09-09 10:43:19 +0900

    Remove fakeairspacechecker, airports.json
    No longer needed since API works

--- server/test/fetch.test.js
+++ server/test/fetch.test.js
@@ -1,5 +1,4 @@
 const AirspaceChecker = require('../lib/AirspaceChecker');
-const FakeAirspaceChecker = require('../lib/FakeAirspaceChecker');
 const assert = require('chai').assert;
 
 describe('AirspaceChecker', () => {
@@ -15,21 +14,5 @@ describe('AirspaceChecker', () => {
     const dest = [36.030399, 138.3148293];
     const result = await AirspaceChecker.checkSpace(origin, dest);
     assert(result === true, 'drone should be allow to fly here');
-  });
-});
-
-xdescribe('FakeAirspaceChecker', () => {
-  it('should not fly in bad zones', async () => {
-    const origin = [35.530596, 139.7401493];
-    const dest = [35.596948, 139.8143323];
-    const result = await FakeAirspaceChecker.checkSpace(origin, dest);
-    assert(result === false, 'it is not ok to fly here');
-  });
-
-  it('should fly in good zones', async () => {
-    const origin = [35.6668205, 139.7132626];
-    const dest = [35.6668205, 139.7131468];
-    const result = await FakeAirspaceChecker.checkSpace(origin, dest);
-    assert(result === true, 'it is ok to fly here');
   });
 });
