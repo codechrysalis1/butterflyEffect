e47485ec0031e88b13ae25424f925692f0f2e96e
Bill Feng <mr.billfeng@gmail.com>
2017-09-09 15:20:01 +0900

    Add distance restriction for get all stations

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,12 +2,27 @@
 /* eslint-disable no-console */
 /* eslint-disable arrow-body-style */
 /* eslint-disable no-await-in-loop */
+/* eslint-disable no-mixed-operators */
 
 const express = require('express');
 const Dijkstra = require('../utils/droneController');
 const uuidv4 = require('uuid/v4');
 
 const router = express.Router();
+
+const distance = (a, b) => {
+  function deg2rad(deg) {
+    return deg * (Math.PI / 180);
+  }
+  const R = 6371; // Radius of the earth in km
+  const dLat = deg2rad(a.lat - b.lat);
+  const dLng = deg2rad(a.lng - b.lng);
+  const A = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(a.lat)) * Math.cos(deg2rad(b.lat)) *
+    Math.sin(dLng / 2) * Math.sin(dLng / 2);
+  const C = 2 * Math.atan2(Math.sqrt(A), Math.sqrt(1 - A));
+  return R * C; // Distance in km
+};
 
 module.exports = (services) => {
   /* GET stations */
@@ -21,11 +36,15 @@ module.exports = (services) => {
         };
       });
       console.log(stations);
-      res.status(200).json(stations);
+      const tokyo = { lat: 35.6895, lng: 139.6917 };
+      res.status(200).json(stations.filter(station =>
+        distance(tokyo, station) < 15,
+      ));
     } catch (err) {
       res.status(400).send('Bad Request');
     }
   });
+
   /* POST calculate */
   router.post('/calculate', (req, res) => {
     const dijkstra = new Dijkstra(req.body.from, req.body.dest, { MAX_DISTANCE: 4 });
@@ -33,6 +52,7 @@ module.exports = (services) => {
     console.log(result);
     res.json(result);
   });
+
   /* POST routes */
   router.post('/routes', async (req, res) => {
     try {
