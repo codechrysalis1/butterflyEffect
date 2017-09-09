a5ebddcd0aaf72382cd1ee87f4ae16721929e01a
haraKumi <email.935@gmail.com>
2017-09-09 20:31:13 +0900

    feature(DB/API): change DB/API design for CI

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,10 +5,11 @@
 /* eslint-disable no-mixed-operators */
 
 const express = require('express');
+const router = express.Router();
 const Dijkstra = require('../utils/droneController');
 const uuidv4 = require('uuid/v4');
+const db = require('../knexfile.js');
 
-const router = express.Router();
 
 const distance = (a, b) => {
   function deg2rad(deg) {
@@ -24,119 +25,151 @@ const distance = (a, b) => {
   return R * C; // Distance in km
 };
 
-module.exports = (services) => {
-  const getStations = async () => {
-    const stations = (await services.db.place.list()).map((station) => {
+const getStations = async () => {
+  const stations = (await db('place').where({ type: 'station' }).select())
+    .map((station) => {
       return {
         id: station.id,
         lat: parseFloat(station.latitude),
         lng: parseFloat(station.longitude),
       };
     });
-    console.log(`All stations: ${stations.length}`);
-    const tokyo = { lat: 35.6895, lng: 139.6917 };
-    return stations.filter(station =>
-      distance(tokyo, station) < 15,
-    );
-  };
 
-  /* GET stations */
-  router.get('/stations', async (req, res) => {
-    try {
-      const stations = await getStations();
-      console.log(`Stations near Tokyo: ${stations.length}`);
-      res.status(200).json(stations);
-    } catch (err) {
-      res.status(400).send('Bad Request');
-    }
-  });
+  console.log(`All stations: ${stations.length}`);
+  const tokyo = { lat: 35.6895, lng: 139.6917 };
+  return stations.filter(station =>
+    distance(tokyo, station) < 15,
+  );
+};
 
-  /* POST calculate */
-  router.post('/calculate', async (req, res) => {
+/* GET stations */
+router.get('/stations', async (req, res) => {
+  try {
     const stations = await getStations();
     console.log(`Stations near Tokyo: ${stations.length}`);
-    const dijkstra = new Dijkstra(req.body.from, req.body.dest, { MAX_DISTANCE: 4 }, stations);
-    const result = dijkstra.solve();
-    console.log('result', result);
-    res.json(result);
-  });
+    res.status(200).json(stations);
+  } catch (err) {
+    res.status(400).send('Bad Request');
+  }
+});
 
-  /* POST routes */
-  router.post('/routes', async (req, res) => {
-    try {
-      const routes = req.body.route;
-      console.log(routes);
-      const trip = {
-        tracknum: uuidv4(),
-        status: 'inprogress',
-      };
+/* POST calculate */
+router.post('/calculate', async (req, res) => {
+  const stations = await getStations();
+  console.log(`Stations near Tokyo: ${stations.length}`);
+  const dijkstra = new Dijkstra(req.body.from, req.body.dest, { MAX_DISTANCE: 4 }, stations);
+  const result = dijkstra.solve();
+  console.log('result', result);
+  res.json(result);
+});
 
-      // store trip
-      const tripResult = await services.db.trip.create(trip);
-      const tripid = tripResult.id;
+/* POST routes */
+router.post('/routes', async (req, res) => {
+  try {
+    const routes = req.body.route;
+    const trip = {
+      tracknum: uuidv4(),
+      status: 'inprogress',
+    };
 
-      for (let i = 0, len = routes.length; i < len; i += 1) {
-        if (i !== len - 1) {
-          const segment = {
-            source_id: routes[i].id,
-            des_id: routes[i + 1].id,
-            trip_id: tripid,
-            drone_id: i + 1,
-          };
-          if (i === 0) {
-            segment.status = 'inprogress';
-          } else {
-            segment.status = 'waiting';
-          }
-          // store segments
-          await services.db.segment.create(segment);
-        }
-
-        if (routes[i].name === 'source' || routes[i].name === 'destination') {
-          const route = {
-            type: routes[i].name,
-            latitude: routes[i].lat,
-            longitude: routes[i].lng,
-          };
-          // store places
-          await services.db.place.create(route);
-        }
-      }
-      const ret = {
-        status: 'success',
+    // store trip
+    const tripResult = await db('trip')
+      .returning('id')
+      .insert({
         tracknum: trip.tracknum,
-      };
-      console.log(ret);
-      res.status(200).json(ret);
-    } catch (err) {
-      res.status(400).json({ status: 'error', message: 'Bad Request' });
-    }
-  });
+        status: trip.status,
+      });
+    console.log('tripResult:', tripResult);
+    const tripid = tripResult.id;
 
-  router.post('/tracknum', async (req, res) => {
-    try {
-      const tracknum = req.body.id;
-      const trip = await services.db.trip.search(tracknum);
-      const tripid = trip.id;
-      const segments = await services.db.segment.search(tripid);
-
-      let telemetry = {};
-      for (let i = 0; i < segments.length; i += 1) {
-        if (segments[i].status === 'inprogress') {
-          const droneId = segments[i].drone_id;
-          telemetry = await services.db.telemetry.search(droneId);
+    for (let i = 0, len = routes.length; i < len; i += 1) {
+      if (i !== len - 1) {
+        const segment = {
+          source_id: routes[i].id,
+          des_id: routes[i + 1].id,
+          trip_id: tripid,
+          drone_id: i + 1,
+        };
+        if (i === 0) {
+          segment.status = 'inprogress';
+        } else {
+          segment.status = 'waiting';
         }
-      }
-      const ret = {
-        route: segments,
-        telemetry,
-      };
-      console.log(ret);
-      res.status(200).json(ret);
-    } catch (err) {
-      res.status(400).json({ status: 'error', message: 'Bad Request' });
-    }
-  });
 
-  return router;
-};
+        // store segments
+        const segmentId = await db('segment')
+          .returning('id')
+          .insert({
+            source_id: segment.source_id,
+            des_id: segment.des_id,
+            trip_id: segment.trip_id,
+            drone_id: segment.drone_id,
+            status: segment.status,
+          });
+        console.log('segment id:', segmentId);
+      }
+
+      if (routes[i].name === 'source' || routes[i].name === 'destination') {
+        const route = {
+          type: routes[i].name,
+          latitude: routes[i].lat,
+          longitude: routes[i].lng,
+        };
+        // store places
+        const placeId = await db('place')
+          .returning('id')
+          .insert({
+            type: route.type,
+            latitude: route.latitude,
+            longitude: route.longitude,
+          });
+        console.log('place id:', placeId);
+      }
+    }
+    const ret = {
+      status: 'success',
+      tracknum: trip.tracknum,
+    };
+    console.log(ret);
+    res.status(200).json(ret);
+  } catch (err) {
+    res.status(400).json({ status: 'error', message: 'Bad Request' });
+  }
+});
+
+/* POST tracknum */
+router.post('/tracknum', async (req, res) => {
+  try {
+    const tracknum = req.body.id;
+    const trip = await db('trip')
+      .where({ tracknum })
+      .select();
+    console.log('trip:', trip);
+    const tripid = trip[0].id;
+    // console.log('obj', { trip_id: tripid })
+    const segments = await db('segment')
+      .where({ trip_id: tripid })
+      .select();
+    console.log('segments:', segments);
+    let telemetry = {};
+    for (let i = 0; i < segments.length; i += 1) {
+      if (segments[i].status === 'inprogress') {
+        const droneId = segments[i].drone_id;
+        telemetry = await db('telemetry')
+          .where({ drone_id: droneId })
+          .select();
+        console.log('telemetry:', telemetry);
+      }
+    }
+    const ret = {
+      routes: segments,
+      telemetry: telemetry[0],
+    };
+    console.log('ret', ret);
+    res.status(200).json(ret);
+  } catch (err) {
+    res.status(400).json({ status: 'error', message: 'Bad Request' });
+  }
+});
+
+module.exports = router;
