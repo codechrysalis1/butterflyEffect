d693fdb60aed84d8e7df941b97e96a0691e9f845
Bill Feng <mr.billfeng@gmail.com>
2017-09-11 16:33:50 +0900

    Add basic front page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,14 @@ import './styles/home.css';
 const Home = () => (
   <div id="home" className="restrict-width">
     <h1>{'Airborne'}</h1>
-    <p>{'World\'s first autonomous drone delivery system.'}</p>
+    <p>{'World\'s first autonomous drone delivery routing system.'}</p>
+    <h2>{'Instructions'}</h2>
+    <ol>
+      <li>{'Enter pickup and drop-off location address into the search bars in \'Send\' tab.'}</li>
+      <li>{'Click \'Search\' to route delivery. Confirm that the path is OK.'}</li>
+      <li>{'Click \'Send\' to start delivery. '}<b>{'Keep tracking number for reference.'}</b></li>
+      <li>{'Enter tracking number into search bar in \'Track\' tab to track the deliery.'}</li>
+    </ol>
   </div>
 );
 
